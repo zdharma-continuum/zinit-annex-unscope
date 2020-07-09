73561fd278c7c68261f479978bb74a8665b57bbf
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2020-07-09 03:09:30 +0200

    Missing code from the to-before-load hook switch.

--- z-a-unscope.plugin.zsh
+++ z-a-unscope.plugin.zsh
@@ -15,7 +15,7 @@ autoload -Uz ∧za-unscope-preinit-handler
 ∧za-unscope-help-null-handler() { :; }
 
 @zinit-register-annex "z-a-unscope" \
-    hook:preinit \
+    hook:before-load \
     ∧za-unscope-preinit-handler \
     ∧za-unscope-help-null-handler \
     "" # No ices
@@ -29,35 +29,35 @@ typeset -gA ZINIT_ANNEX_UNSCOPE_MAPPINGS
 
 ZINIT_ANNEX_UNSCOPE_MAPPINGS=(
     # z-a-as-monitor
-    "z-a-as-monitor"	        "zinit-zsh/z-a-as-monitor p"
-    "as-monitor"	        "zinit-zsh/z-a-as-monitor p"
-    "monitor"		        "zinit-zsh/z-a-as-monitor p"
+    "z-a-as-monitor"	        "zinit-zsh/z-a-as-monitor 0"
+    "as-monitor"	        "zinit-zsh/z-a-as-monitor 0"
+    "monitor"		        "zinit-zsh/z-a-as-monitor 0"
                                 
     # z-a-patch-dl              
-    "z-a-patch-dl"	        "zinit-zsh/z-a-patch-dl p"
-    "patch-dl"		        "zinit-zsh/z-a-patch-dl p"
+    "z-a-patch-dl"	        "zinit-zsh/z-a-patch-dl 0"
+    "patch-dl"		        "zinit-zsh/z-a-patch-dl 0"
                                 
     # z-a-rust                  
-    "z-a-rust"		        "zinit-zsh/z-a-rust p"
-    "rust"		        "zinit-zsh/z-a-rust p"
+    "z-a-rust"		        "zinit-zsh/z-a-rust 0"
+    "rust"		        "zinit-zsh/z-a-rust 0"
                                 
     # z-a-bin-gem-node          
-    "z-a-bin-gem-node"	        "zinit-zsh/z-a-bin-gem-node p"
-    "bin-gem-node"	        "zinit-zsh/z-a-bin-gem-node p"
-    "bgn"		        "zinit-zsh/z-a-bin-gem-node p"
+    "z-a-bin-gem-node"	        "zinit-zsh/z-a-bin-gem-node 0"
+    "bin-gem-node"	        "zinit-zsh/z-a-bin-gem-node 0"
+    "bgn"		        "zinit-zsh/z-a-bin-gem-node 0"
                                 
     # fast-syntax-highlighting  
-    "fast-syntax-highlighting"  "zdharma/fast-syntax-highlighting p"
-    "f-sy-h"		        "zdharma/fast-syntax-highlighting p"
-    "fsh"		        "zdharma/fast-syntax-highlighting p"
+    "fast-syntax-highlighting"  "zdharma/fast-syntax-highlighting 0"
+    "f-sy-h"		        "zdharma/fast-syntax-highlighting 0"
+    "fsh"		        "zdharma/fast-syntax-highlighting 0"
 
     # history-search-multi-word
-    "history-search-multi-word" "zdharma/history-search-multi-word p"
-    "hsmw"		        "zdharma/history-search-multi-word p"
+    "history-search-multi-word" "zdharma/history-search-multi-word 0"
+    "hsmw"		        "zdharma/history-search-multi-word 0"
 
     # Prezto environment
-    "environment"		"PZTM::environment s"
-    "env"			"PZTM::environment s"
+    "environment"		"PZTM::environment 1"
+    "env"			"PZTM::environment 1"
 )
 
 # vim:ft=zsh:tw=80:sw=4:sts=4:noet
