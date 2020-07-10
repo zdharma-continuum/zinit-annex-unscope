6db089fbd560f1339d02e317e25f5221222527b4
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2020-07-10 10:06:04 +0200

    --list/-l, .za-unscope-list-mappings.

--- z-a-unscope.plugin.zsh
+++ z-a-unscope.plugin.zsh
@@ -15,7 +15,8 @@ Zinit_Annex_Unscope[0]="$0" Zinit_Annex_Unscope[repo-dir]="${0:h}"
 autoload -Uz ∧za-unscope-before-load-handler \
     ∧za-unscope-scope-cmd-help-handler \
     ∧za-unscope-scope-cmd \
-    .za-scope-dynamic
+    .za-scope-dynamic \
+    .za-unscope-list-mappings
 
 # An empty stub to fill the help handler fields
 ∧za-unscope-help-null-handler() { :; }
